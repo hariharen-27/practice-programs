655c22a8e778f602f546c7fe4840fae0fd595df0
hariharen-27 <hariharen27302@gmail.com>
2023-03-11 02:09:19 +0530

    added pattern problems

--- pattern/patterns/src/pattern5.java
+++ pattern/patterns/src/pattern5.java
@@ -2,7 +2,10 @@ public class pattern5  {
     public static void main(String[] args){
         int n=9;
         for (int rows = 0; rows < n; rows++) {
-
+            for (int cols=0;cols<n/2;cols++){
+                
+            }
+            System.out.println();
         }
     }
 }
