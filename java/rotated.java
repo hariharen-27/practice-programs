d8fa73cc14522803ace99e0e98be767e02d57c15
hariharen-27 <hariharen27302@gmail.com>
2023-11-28 09:54:10 +0530

    added rotated.java

--- java/rotated.java
+++ java/rotated.java
@@ -3,7 +3,7 @@ public class rotated {
     public static void main(String[] args){
         int a[]={1,2,3,4,5,6,7,8,9};
         int k=4;
-
+        k=k%a.length;
         
                 reverse(a,0,k-1);
                 reverse(a,k,a.length-1);
